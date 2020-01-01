8b2db790c8442c4b0290d3580932679058d714ef
Mccxiv <adr.stella@gmail.com>
2020-01-01 17:29:26 +0800

    Try to queue requests

---
+++ src/lib/request-queue.js
@@ -0,0 +1,19 @@
+import { respond } from './request-handlers'
+
+let currentReqRes = null
+const reqResQueue = []
+
+setInterval(handleNextRequestMaybe, 50)
+
+export function queueRequest (req, res) {
+  reqResQueue.push({req, res})
+}
+
+async function handleNextRequestMaybe () {
+  if (!currentReqRes && reqResQueue.length) {
+    console.log(`Responding... There are ${reqResQueue.length} requests pending`)
+    currentReqRes = reqResQueue.shift()
+    try { await respond(currentReqRes.req, currentReqRes.res) }
+    finally { currentReqRes = null }
+  }
+}
--- src/lib/server.js
+++ src/lib/server.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import {allowCORS} from './helpers';
-import {logRequest, joinChannel, respond} from './request-handlers';
+import {logRequest, joinChannel} from './request-handlers';
+import {queueRequest} from './request-queue'
 
 export function startServer(port) {
   console.log('Server starting');
   const app = express();
   app.use(allowCORS);
-  app.get('/v1/:channel', logRequest, joinChannel, respond);
+  app.get('/v1/:channel', logRequest, joinChannel, queueRequest);
   app.set('json spaces', 2);
   app.listen(port);
 }
