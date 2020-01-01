cfe9f83403838753e1b190fdd080583f3b8c7001
Mccxiv <adr.stella@gmail.com>
2020-01-01 17:30:17 +0800

    Try to queue requests

--- dist/lib/server.js
+++ dist/lib/server.js
@@ -13,13 +13,15 @@ var _helpers = require('./helpers');
 
 var _requestHandlers = require('./request-handlers');
 
+var _requestQueue = require('./request-queue');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function startServer(port) {
   console.log('Server starting');
   var app = (0, _express2.default)();
   app.use(_helpers.allowCORS);
-  app.get('/v1/:channel', _requestHandlers.logRequest, _requestHandlers.joinChannel, _requestHandlers.respond);
+  app.get('/v1/:channel', _requestHandlers.logRequest, _requestHandlers.joinChannel, _requestQueue.queueRequest);
   app.set('json spaces', 2);
   app.listen(port);
 }
